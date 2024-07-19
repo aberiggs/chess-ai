c89eea9255f34f810dcafa9fb98a8cbcec8168ec
aberiggs <sprocket710@gmail.com>
2024-07-18 23:30:35 -0700

    Model comments ✏️

---
+++ model/model.py
@@ -0,0 +1,10 @@
+"""
+8x8x12 convolutional neural network
+- 8x8 board
+- 6 different types of pieces
+- 2 colors
+
+Will output a 8x8x8x8 tensor representing the probability of each piece being moved from a position to another
+- From position within the first 8x8 board
+- To position within the second 8x8 board
+"""
